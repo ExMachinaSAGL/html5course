ca9181d8e5df6c5b1d7579df6de85aac62ecda2f
Federico Ghedina <fedeghe@gmail.com>
2014-03-07 01:16:31 +0100

    exercise 3 added, really basic validation with assistance plugin, to be completed

--- 2/exercises/2-3/js/init.js
+++ 2/exercises/2-3/js/init.js
@@ -3,5 +3,27 @@ jQuery(function (){
 
     
 
+    $("#form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            url: {
+                required: true,
+                url: true
+            },
+            age: {
+                required: true
+            },
+            product: {
+                required: true
+            },
+            from: {
+                required: true
+            }
+        }
+    });
+
 //end dom ready
 });
